bd31821918e1bc42b1bee185cc21c2d2990621a2
Trismegiste <flo@trismegiste.net>
2015-07-25 05:12:49 +0200

    pricing style

--- src/Repository/Plan.php
+++ src/Repository/Plan.php
@@ -20,7 +20,24 @@ class Plan
                 'db_limit' => 2,
                 'storage_limit' => 2,
                 'bandwidth_limit' => 20,
-                'descr' => 'wesh wesh'
+                'descr' => 'wesh wesh',
+                'price' => 19
+            ],
+            [
+                'name' => 'value',
+                'db_limit' => 10,
+                'storage_limit' => 20,
+                'bandwidth_limit' => 300,
+                'descr' => 'wesh wesh',
+                'price' => 49
+            ],
+            [
+                'name' => 'large',
+                'db_limit' => 50,
+                'storage_limit' => 100,
+                'bandwidth_limit' => 1000,
+                'descr' => 'wesh wesh',
+                'price' => 199
             ]
         ];
     }
