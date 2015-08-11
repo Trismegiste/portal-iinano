6fcb42519ddf7a44e18e566aaf2d265ddc8e2ff7
Trismegiste <flo@trismegiste.net>
2015-08-11 22:00:52 +0200

    func test for order checkout

--- src/Tests/Controller/OrderControllerTest.php
+++ src/Tests/Controller/OrderControllerTest.php
@@ -12,13 +12,22 @@ namespace Trismegiste\PortalBundle\Tests\Controller;
 class OrderControllerTest extends ControllerTestCase
 {
 
-    public function testCheckout()
+    public function testCheckoutWithoutAuth()
     {
         $this->client->followRedirects();
         $crawler = $this->client->request('GET', $this->generateUrl('front_plan_list'));
         $button = $crawler->selectButton('Choose');
         $this->client->submit($button->form());
+        $this->assertInstanceOf('Trismegiste\PortalBundle\Model\Order', $this->client->getRequest()->getSession()->get('order'));
         $this->assertRoute('trismegiste_oauth_connect');
+    }
+
+    public function testCheckoutWithPreviousAuth()
+    {
+        $crawler = $this->client->request('GET', $this->generateUrl('trismegiste_oauth_connect'));
+        $link = $crawler->filter('i[class="icon-dummy-sign"]')->parents()->link();
+        $crawler = $this->client->click($link);
+        $crawler->selectButton('Redirect');
     }
 
 }
